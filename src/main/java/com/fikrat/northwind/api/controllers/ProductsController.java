b88746eb378601b68caab628ba5f55ecc76177d1
Fikrat Mammadov <fikrat.mammadov.a@asoiu.edu.az>
2022-02-10 01:39:32 +0400

    first commit

---
+++ src/main/java/com/fikrat/northwind/api/controllers/ProductsController.java
@@ -0,0 +1,67 @@
+package com.fikrat.northwind.api.controllers;
+
+import com.fikrat.northwind.business.abstracts.ProductService;
+import com.fikrat.northwind.core.utilities.results.DataResult;
+import com.fikrat.northwind.core.utilities.results.Result;
+import com.fikrat.northwind.entities.concretes.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products/")
+public class ProductsController {
+    private ProductService productService;
+
+    @Autowired
+    public ProductsController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("getall")
+    public DataResult<List<Product>> getAll(){
+        return productService.getAll();
+    }
+
+    @PostMapping ("add")
+    public Result add(@RequestBody Product product){
+        return this.productService.add(product);
+    }
+
+    @GetMapping("getbyproductname")
+    public DataResult<Product> getByProductName(@RequestParam String productName){
+        return this.productService.getByProductName(productName);
+    }
+
+    @GetMapping("getByProductNameAndCategoryId")
+    public DataResult<Product> getByProductNameAndCategoryId(@RequestParam("productName") String productName,
+                                                             @RequestParam("categoryId") int categoryId){
+        return this.productService.getByProductNameAndCategoryId(productName,categoryId);
+    }
+
+    @GetMapping("getByProductNameOrCategoryId")
+    DataResult<List<Product>> getByProductNameOrCategoryId(@RequestParam String productName,int categoryId){
+        return this.productService.getByProductNameOrCategoryId(productName,categoryId);
+    }
+
+    @GetMapping("getByCategoryIdIn")
+    DataResult<List<Product>> getByCategoryIdIn(@RequestParam List<Integer> categories){
+        return this.productService.getByCategoryIdIn(categories);
+    }
+
+    @GetMapping("getByProductNameContains")
+    DataResult<List<Product>> getByProductNameContains(@RequestParam String productName){
+        return this.productService.getByProductNameContains(productName);
+    }
+
+    @GetMapping("getAllByPage")
+    public DataResult<List<Product>> getAll(int pageNo, int pageSize){
+        return this.productService.getAll(pageNo-1,pageSize);
+    }
+
+    @GetMapping("getAllDesc")
+    public DataResult<List<Product>> getAllSorted(){
+        return this.productService.getAllSorted();
+    }
+}
---
+++ src/main/java/com/fikrat/northwind/business/concretes/ProductManager.java
@@ -0,0 +1,84 @@
+package com.fikrat.northwind.business.concretes;
+
+import com.fikrat.northwind.business.abstracts.ProductService;
+import com.fikrat.northwind.core.utilities.results.DataResult;
+import com.fikrat.northwind.core.utilities.results.Result;
+import com.fikrat.northwind.core.utilities.results.SuccessDataResult;
+import com.fikrat.northwind.core.utilities.results.SuccessResult;
+import com.fikrat.northwind.dataAccess.abstracts.ProductDao;
+import com.fikrat.northwind.entities.concretes.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductManager implements ProductService {
+    private ProductDao productDao;
+
+    @Autowired
+    public ProductManager(ProductDao productDao) {
+        this.productDao = productDao;
+    }
+
+    @Override
+    public DataResult<List<Product>> getAll() {
+        return new SuccessDataResult<List<Product>>(productDao.findAll(),"data listelendi.");
+    }
+
+    @Override
+    public DataResult<List<Product>> getAllSorted() {
+        Sort sort = Sort.by(Sort.Direction.DESC,"productName");
+        return new SuccessDataResult<List<Product>>(this.productDao.findAll(sort));
+    }
+
+    @Override
+    public DataResult<List<Product>> getAll(int pageNo, int pageSize) {
+        Pageable pageable= PageRequest.of(pageNo,pageSize);
+        return new SuccessDataResult<List<Product>>(this.productDao.findAll(pageable).getContent());
+    }
+
+    @Override
+    public Result add(Product product) {
+        this.productDao.save(product);
+        return new SuccessResult("Urun eklendi");
+    }
+
+    @Override
+    public DataResult<Product> getByProductName(String productName) {
+        return new SuccessDataResult<Product>(productDao.getByProductName(productName),"data listelendi");
+    }
+
+    @Override
+    public DataResult<Product> getByProductNameAndCategoryId(String productName, int categoryId) {
+        return new SuccessDataResult<Product>(productDao.getByProductNameAndCategory_CategoryId(productName,categoryId),"data listelendi");
+    }
+
+    @Override
+    public DataResult<List<Product>> getByProductNameOrCategoryId(String productName, int categoryId) {
+        return new SuccessDataResult<List<Product>>(productDao.getByProductNameOrCategory_CategoryId(productName,categoryId),"data listelendi.");
+    }
+
+    @Override
+    public DataResult<List<Product>> getByCategoryIdIn(List<Integer> categories) {
+        return new SuccessDataResult<List<Product>>(productDao.getByCategory_CategoryIdIn(categories),"data listelendi.");
+    }
+
+    @Override
+    public DataResult<List<Product>> getByProductNameContains(String productName) {
+        return new SuccessDataResult<List<Product>>(productDao.getByProductNameContains(productName),"data listelendi.");
+    }
+
+    @Override
+    public DataResult<List<Product>> getByProductNameStartsWith(String productName) {
+        return new SuccessDataResult<List<Product>>(productDao.getByProductNameStartsWith(productName),"data listelendi.");
+    }
+
+    @Override
+    public DataResult<List<Product>> getByNameAndCategory(String productName, int categoryId) {
+        return new SuccessDataResult<List<Product>>(productDao.getByNameAndCategory_CategoryId(productName,categoryId),"data listelendi.");
+    }
+}
